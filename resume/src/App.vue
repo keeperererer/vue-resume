063a6c25b044b423b651ecc29872f757d8bbb97f
keeperererer <2249269086@qq.com>
2019-05-29 13:17:18 +0800

    整体框架

---
+++ resume/src/App.vue
@@ -0,0 +1,124 @@
+<template>
+  <div id="app">
+    <ShowMenu v-on:listenShowMenu='listenShowMenu'></ShowMenu>
+    <!-- <ShowStyle ref="comShowStyle"></ShowStyle>
+    <ShowResume ref="comShowResume" :resumeData="resumeData"></ShowResume>
+    <transition name="fade">
+        <FormList 
+        ref="comFormList" 
+        :resumeData="resumeData" 
+        v-if="formListShow"
+        v-on:listenFormList='listenFormList'>
+        </FormList>
+    </transition> -->
+  </div>
+</template>
+
+<script>
+import ShowMenu from './components/showMenu/showMenu.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ShowMenu
+  }
+}
+</script>
+
+<style>
+import 
+    /* 内外边距通常让各个浏览器样式的表现位置不同 */
+    body, div, dl, dt, dd, ul, ol, li, h1, h2, h3, h4, h5,
+    h6, pre, code, form, fieldset, legend, input, textarea,
+    p, blockquote, th, td, hr, button, article, aside, details,
+    figcaption, figure, footer, header, menu, nav, section {
+        margin: 0;
+        padding: 0;
+    }
+
+    input, select, textarea {
+        font-size: 100%;
+    }
+
+    input {
+        outline: none;
+        border-style: none
+    }
+
+    /* 去掉各 Table  cell 的边距并让其边重合 */
+    table {
+        border-collapse: collapse;
+        border-spacing: 0;
+    }
+
+    /* 去除默认边框 */
+    fieldset, img {
+        border: 0;
+    }
+
+    /* 去掉 firefox 下此元素的边框 */
+    abbr, acronym {
+        border: 0;
+        font-variant: normal;
+    }
+
+    /* 一致的 del 样式 */
+    del {
+        text-decoration: line-through;
+    }
+
+    address, caption, cite, code, dfn, em, th, var {
+        font-style: normal;
+        font-weight: 500;
+    }
+
+    /* 去掉列表前的标识, li 会继承 */
+    ol, ul {
+        list-style: none;
+    }
+
+    /* 对齐是排版最重要的因素, 别让什么都居中 */
+    caption, th {
+        text-align: left;
+    }
+
+    a {
+        text-decoration: none;
+    }
+
+    /*清除浮动代码*/
+    .clearfloat:after {
+        display: block;
+        clear: both;
+        content: "";
+        visibility: hidden;
+        height: 0
+    }
+
+    .clearfloat {
+        zoom: 1
+    }
+
+    body, html {
+        width: 100%;
+        height: 100%;
+        padding: 0;
+        margin: 0;
+        min-width: 1024px;
+        font-size: 62.5%;
+    }
+
+    #app {
+        width: 100%;
+        height: 100%;
+    }
+
+    .fade-enter-active, .fade-leave-active {
+        transition: opacity .5s
+    }
+
+    .fade-enter, .fade-leave-active {
+        opacity: 0
+    }
+</style>
+
---
+++ resume/src/components/showMenu/showMenu.vue
@@ -0,0 +1,62 @@
+<template>
+	<div id="show-menu">
+		<div class="menu-cont">
+			<div class="logo">
+				<a href="https://github.com/keeperererer/vue-resume" target="_blank">
+					<i class="icon-github icon-5x"></i>
+				</a>
+			</div>
+			<div class="file" @click="fileClick">
+				<i class=" icon-file-alt icon-5x"></i>
+	            <p class="file-des">生成简历</p>
+			</div>
+			<div class="download-file" @click="downloadClick">
+				<i class=" icon-download-alt icon-5x"></i>
+			</div>
+			<transition name="fade">
+            <div class="download-choice" v-show="showDownloadChoice">
+                <div class="dpi96 setdpi">
+                    <p>96像素/英寸</p>
+                    <p>A4 794×1123</p>
+                    <input type="radio" @click="choiceClick({'width':794,'height':1123})">
+                </div>
+                <div class="dpi120 setdpi">
+                    <p>120像素/英寸</p>
+                    <p>A4 1487×2105</p>
+                    <input type="radio" @click="choiceClick({'width':1487,'height':2015})">
+                </div>
+                <div class="custom">
+                    width :<input type="text" v-model="customPx.width"  placeholder="px">
+                    height:<input type="text" v-model="customPx.height"  placeholder="px">
+                    <span class="ok-choice" @click="choiceClick({'width':parseInt(customPx.width),'height':parseInt(customPx.height)})">down</span>
+                </div>
+            </div>
+            </transition>
+		</div>
+	</div>
+</template>
+
+<script>
+export default{
+	data() {
+		return {
+			msg:'hello vue',
+			showDownloadChoice:false,
+			customPx:{
+				width:"",
+				height:""
+			}
+		}
+	},
+	created(){
+
+	},
+	methods: {
+		
+	}
+}
+</script>
+
+<style>
+
+</style>
